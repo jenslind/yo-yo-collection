9737c2f76d4a069b619eb334eee2b9805eaed894
Jens Lind <jens@jensl.se>
2017-05-15 09:39:07 +0200

    Do not use es6 stuff

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const yo = require('yo-yo')
-const nanobus = require('nanobus')
+var yo = require('yo-yo')
+var nanobus = require('nanobus')
 
-const Collection = function (state) {
+var Collection = function (state) {
   this.state = state || {}
   this.bus = nanobus()
 }
 
 Collection.prototype.component = function (c, options) {
+  var self = this
+
   if (!options) options = {}
 
-  let el = c(this.state, this.bus)
+  var el = c(this.state, this.bus)
   if (options.mount) document.querySelector(options.mount).appendChild(el)
 
-  this.bus.on('rerender', () => {
-    yo.update(el, c(this.state, this.bus), options)
+  this.bus.on('rerender', function () {
+    yo.update(el, c(self.state, self.bus), options)
   })
 }
 
